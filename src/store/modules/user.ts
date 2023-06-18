5d2a7809578c789f28d97b3c13e9ff1905af48b8
zhaomin <2682824028@qq.com>
2023-06-18 15:07:58 +0800

    路由

--- src/store/modules/user.ts
+++ src/store/modules/user.ts
@@ -2,15 +2,18 @@ import type { MutationTree, ActionTree, GetterTree } from 'vuex'
 import type { RootState } from '../index'
 import { setItem, getItem } from '@/utils/storage'
 import { fetchUserProfile } from '@/api/system'
+import type { RouteRecordRaw } from 'vue-router'
 
 export interface UserState {
   token: string | null
   userInfo: any
+  currentRoutes: RouteRecordRaw[]
 }
 
 const state: UserState = {
   token: getItem('token') || null,
-  userInfo: null
+  userInfo: null,
+  currentRoutes: []
 }
 
 const getters: GetterTree<UserState, RootState> = {}
