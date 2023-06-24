882e7973658ffb81be607abf475b5231ec1df150
zhaomin <2682824028@qq.com>
2023-06-24 14:48:49 +0800

    feat: 增加下载流文件

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,5 @@
 import type { CommonAttrs } from '@/types'
+import axios from 'axios'
 
 // 判断是否网络地址
 export const hasHttp = (url: string): boolean => {
@@ -48,3 +49,72 @@ export const traversalTree = (list: Item[], minLevel: number) => {
 
   return result
 }
+
+type Options = {
+  url: string
+  method: 'get' | 'post' | 'GET' | 'POST'
+  fileName: string
+  paramsOrData: CommonAttrs
+  blobType: string
+  responseType: string
+  headers: CommonAttrs
+}
+
+// 下载流文件
+export const downloadStreamFile = ({
+  url,
+  method = 'GET',
+  fileName = '',
+  paramsOrData,
+  blobType,
+  responseType = 'blob', // blob 就是字节流
+  headers = {}
+}: Options) => {
+  const q: CommonAttrs = {
+    url,
+    method,
+    responseType,
+    headers: {
+      ...headers
+    }
+  }
+  if (method.toLocaleLowerCase() === 'get') {
+    q.params = paramsOrData
+  } else {
+    q.data = paramsOrData
+  }
+
+  return axios(q)
+    .then(resp => {
+      const blob = blobType ? new Blob([resp.data], { type: blobType }) : new Blob([resp.data])
+      const href = window.URL.createObjectURL(blob)
+      const aElm = document.createElement('a')
+      aElm.href = href
+      aElm.setAttribute('download', fileName)
+      document.body.appendChild(aElm)
+      aElm.click()
+      window.URL.revokeObjectURL(href)
+      aElm.remove()
+    })
+}
+
+/*
+  下载 zip 文件
+  downloadStreamFile({
+    url: '/ywXjjl/filePack',
+    method: 'POST',
+    paramsOrData: this.params,
+    blobType: 'application/zip',
+    fileName: `${fileName}.zip`,
+    headers: {
+      'Content-Type': 'application/json; application/octet-stream' // 字节流
+    }
+  })
+    .then(() => {
+      this.downloadClose()
+    })
+    .catch(() => {
+      this.loadingFile = false
+      this.$message.error('下载错误')
+    })
+*/
